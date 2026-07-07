6361f29461fa8f97f698b5b62b73e06d87b04a44
Harshit Bisht <harshitbishtdelhi@gmail.com>
2026-07-07 10:56:05 +0530

    Structure exercises

--- LeanLangur/People.lean
+++ LeanLangur/People.lean
@@ -57,6 +57,19 @@ def bob : Voter := -- defines `bob`
 Define a structure `EvenNumber` that represents an even natural number. It should have a field `value : Nat` and a proof that `value` is even (i.e., there exists some `k : Nat` such that `value = 2 * k`). Then, create an instance of `EvenNumber` for the number 10. Also define a function ``double: Nat → EvenNumber`` that takes a natural number `n` and returns an `EvenNumber` representing `2 * n`.
 -/
 
+structure EvenNumber where
+  value : Nat
+  is_even : ∃ (k : Nat), value = 2 * k
+  deriving Repr, DecidableEq
+
+def num : EvenNumber :=
+  {value := 10, is_even := ⟨5, by simp⟩}
+
+def double (num : Nat) : EvenNumber :=
+  {value := 2*num, is_even := ⟨num, by simp⟩}
+
+#eval double 3
+
 end langur -- closes the current namespace or section
 /-!
 ## Next files
